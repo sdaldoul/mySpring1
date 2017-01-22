02dab92aa74614511f4eef0e0e29cd78e90a61ff
sdaldoul <daldoul.slaheddine@gmail.com>
2017-01-22 22:24:26 +0100

    Authentification from database

--- src/main/java/org/sid/sec/SecurityConfig.java
+++ src/main/java/org/sid/sec/SecurityConfig.java
@@ -1,6 +1,10 @@
 package org.sid.sec;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,10 +14,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN", "USER");
-		auth.inMemoryAuthentication().withUser("user").password("1234").roles("USER");
+		/*
+		 * auth.inMemoryAuthentication().withUser("admin").password("1234").
+		 * roles("ADMIN", "USER");
+		 * auth.inMemoryAuthentication().withUser("user").password("1234").roles
+		 * ("USER");
+		 */
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(
+						"select username as principal, password as credentials, active from users where username=?")
+				.authoritiesByUsernameQuery(
+						"select username as principal, role as role from users_roles where username=?")
+				.rolePrefix("ROLE_").passwordEncoder(new Md5PasswordEncoder());
+
 	}
 
 	@Override
@@ -22,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/consulterCompte", "/operations").hasRole("USER");
 		http.authorizeRequests().antMatchers("/saveOperation").hasRole("ADMIN");
 		http.exceptionHandling().accessDeniedPage("/403");
-		
+
 	}
 
 }
